b98de848fe0ebe87627606f91924a543945bdf19
Adrien Driffaud <adrien@driffaud.fr>
2022-07-02 10:30:51 +0200

    Project init

---
+++ lib/ray_tracer_challenge.ex
@@ -0,0 +1,18 @@
+defmodule RayTracerChallenge do
+  @moduledoc """
+  Documentation for `RayTracerChallenge`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RayTracerChallenge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
